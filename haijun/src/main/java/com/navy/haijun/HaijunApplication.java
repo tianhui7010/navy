ce984f811f2afb575c97acbbc581c9518b87ff14
tianhui <526136300@qq.com>
2020-10-13 10:55:17 +0800

    监控中心，测试更新代码

--- haijun/src/main/java/com/navy/haijun/HaijunApplication.java
+++ haijun/src/main/java/com/navy/haijun/HaijunApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//监控中心
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 public class HaijunApplication {
